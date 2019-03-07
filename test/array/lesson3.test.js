7d63e747dfce73de74eb1fdadee192efc4fe71a8
qiqi <13759525126@163.com>
2019-03-07 15:22:25 +0800

    数组

---
+++ test/array/lesson3.test.js
@@ -0,0 +1,6 @@
+import flower from '../../code/array/lesson3'
+
+test('flower:[1,0,0,0,1]', () => {
+  expect(flower([1, 0, 0, 0, 1], 1)).toBe(true)
+  expect(flower([1, 0, 0, 0, 1], 2)).toBe(false)
+})
---
+++ test/array/lesson4.test.js
@@ -0,0 +1,5 @@
+import grayCode from '../../code/array/lesson4'
+
+test('grayCode', () => {
+  expect(grayCode(2)).toEqual(['00', '01', '11', '10'])
+})
